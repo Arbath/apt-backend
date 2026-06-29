538f413f68dcf44143ccbdf412bc779ea731785e
Arbath <arbath@teknohole.com>
2026-06-29 21:36:16 +0700

    fix(users): fix typo in database migration

--- migrations/20260629140303_rename_enum_assesor.sql
+++ migrations/20260629140303_rename_enum_assesor.sql
@@ -1,3 +1,3 @@
 -- Add migration script here
-ALTER TYPE role_users RENAME VALUE 'asesor' TO 'ASSESSOR';
+ALTER TYPE role_users RENAME VALUE 'asesor' TO 'assessor';
 ALTER TABLE users ALTER COLUMN role SET DEFAULT 'auditee';
